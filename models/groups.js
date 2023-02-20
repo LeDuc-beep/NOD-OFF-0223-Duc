c2b1f94667109f7ec5f609ad7769a18d79a33fc3
leduc78919 <leduc78919@gmail.com>
2023-02-20 10:12:26 +0700

    2.1

---
+++ models/groups.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseDelete = require("mongoose-delete");
+const {Types} = require("mongoose");
+
+const itemsSchema = new Schema(
+  {
+    name: {
+        type: String,
+        maxLength: 255,
+        default: '',
+    },
+    status: {
+        type: String,
+        default: '',
+    },
+    ordering: {
+        type: Number,
+        default: 0,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    createdBy: {
+        idUser: {
+            type: String,
+            default: '',
+        },
+        userName: { type: String, default: '' },
+    },
+    modifiedBy: {
+        idUser: String,
+        userName: String,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+itemsSchema.plugin(mongooseDelete, { overrideMethods: "all", deletedAt: true });
+
+const items = mongoose.model("items", itemsSchema);
+module.exports = items;
